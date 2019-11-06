2409e59dcf014189431a22ae102508502e140671
Vivek Dubey <vatsa.vivek@gmail.com>
2019-11-06 20:51:07 +1100

    Added authentication

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const users = require('../controllers/users');
 const {ensureLoggedIn } = require('connect-ensure-login')
 const { isAuthorized } = require('../middleware/authorized');
 
-userRouter.get('/', users.getUsers);
+userRouter.get('/', ensureLoggedIn('/auth'), users.getUsers);
 userRouter.post('/delete', ensureLoggedIn('/auth'), isAuthorized, users.deleteUser);
 userRouter.post('/add', ensureLoggedIn('/auth'), isAuthorized, users.addUser);
 userRouter.get('/add', users.getAddForm);
